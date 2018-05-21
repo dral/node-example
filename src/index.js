682b2f0b4dbb4a093d5d452db9ee2c0fda5ecefe
Draltan Marin <draltan.marin@gmail.com>
2018-05-21 14:42:09 +0200

    feat: add template, setup project syntax and build settings

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.engine('pug', require('pug').__express);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/david', function (req, res) {
+  res.render('test.pug', req.query);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
